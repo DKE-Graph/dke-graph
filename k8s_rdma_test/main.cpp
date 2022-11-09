722bb52371f2810115f8a86f518e1075cca28cc7
laewonjeong <laewonjeong@kangwon.ac.kr>
2022-11-09 16:13:05 +0900

    really error correction

--- k8s_rdma_test/main.cpp
+++ k8s_rdma_test/main.cpp
@@ -13,42 +13,43 @@ char send_buffer[num_of_node][buf_size];
 char recv_buffer[num_of_node][buf_size];
 
 bool is_server(string ip){
-  if(ip == server_ip)
-    return true;
-  return false;
+    if(ip == server_ip)
+        return true;
+    return false;
 }
 
 int main(int argc, char* argv[]){
-  if(argc != 2)
-  {
-    cerr << argv[0] << " <MY IP> " << endl;
-    exit(1);
-  }
-  if(server_ip != node[0]){
-    cerr << "node[0] is not server_ip" << endl;
-    exit(1);
-  }
-  myRDMA myrdma;
+    if(argc != 2)
+    {
+        cerr << argv[0] << " <MY IP> " << endl;
+        exit(1);
+    }
+    if(server_ip != node[0]){
+        cerr << "node[0] is not server_ip" << endl;
+        exit(1);
+    }
+    myRDMA myrdma;
 
-  myrdma.initialize_rdma_connection(argv[1], node, num_of_node, port,send_buffer,recv_buffer);
+    myrdma.initialize_rdma_connection(argv[1], node, num_of_node, port,send_buffer,recv_buffer);
  
-  myrdma.create_rdma_info();
-  myrdma.send_info_change_qp();
+    myrdma.create_rdma_info();
+    myrdma.send_info_change_qp();
 
-  cerr << "====================================================" << endl;
+    cerr << "====================================================" << endl;
   
-  string ip = argv[1];
-  string msg;
-  string opcode = "send"; //send, send_with_imm, write, write_with_imm
+    string ip = argv[1];
+    string msg;
+    string opcode = "send"; //send, send_with_imm, write, write_with_imm
 
-  if (strcmp(argv[2],server_ip) == 0){
-    cin >> msg;
-    myrdma.rdma_send(msg,0);
-  }
-  else{
-    myrdma.rdma_send_recv(0);
-    cerr << recv_buffer[0] << endl;
-  }
+    cerr << strcmp(argv[1],server_ip) << endl;
+    if (strcmp(argv[1],server_ip) == 0){
+        cin >> msg;
+        myrdma.rdma_send(msg,0);
+    }
+    else{
+        myrdma.rdma_send_recv(0);
+        cerr << recv_buffer[0] << endl;
+    }
   
 
   myrdma.exit_rdma();
