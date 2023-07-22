13d80640656dc1d544cd953c2e4d9da8594f153b
HanYoonSoo <hdbstn3055@gmail.com>
2023-07-22 16:24:15 +0900

    Bucket CRUD

--- src/main/java/com/etri/sodasapi/rgw/RGWService.java
+++ src/main/java/com/etri/sodasapi/rgw/RGWService.java
@@ -3,9 +3,13 @@ package com.etri.sodasapi.rgw;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
@@ -30,18 +34,10 @@ public class RGWService {
     @Value("${RGW_ENDPOINT}")
     public String RGW_ENDPOINT;
 
+    private static AmazonS3 amazonS3;
+
     public List<SBucket> getBuckets(Key key) {
-        String accessKey = key.getAccessKey();
-        String secretKey = key.getSecretKey();
-
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        clientConfig.setProtocol(Protocol.HTTP);
-
-        AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
-
-        conn.setEndpoint(RGW_ENDPOINT);
+        AmazonS3 conn = getClient(key);
 
         List<Bucket> buckets = conn.listBuckets();
         List<SBucket> bucketList = new ArrayList<>();
@@ -58,60 +54,51 @@ public class RGWService {
     }
 
     public List<BObject> getObjects(Key key, String bucketName) {
-        String accessKey = key.getAccessKey();
-        String secretKey = key.getSecretKey();
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3 conn = getClient(key);
 
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        clientConfig.setProtocol(Protocol.HTTP);
-
-        AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
-        conn.setEndpoint(RGW_ENDPOINT);
-
-        ObjectListing objects = conn.listObjects("test-bucket");
+        ObjectListing objects = conn.listObjects(bucketName);
 
 //        System.out.println(objects);
         List<BObject> objectList = new ArrayList<>();
-//
-//        do {
-//            for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
-//                objectList.add(new BObject(objectSummary.getKey(), objectSummary.getSize(), objectSummary.getLastModified()));
-//            }
-//            objects = conn.listNextBatchOfObjects(objects);
-//        } while (objects.isTruncated());
+
+        do {
+            for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+                objectList.add(new BObject(objectSummary.getKey(), objectSummary.getSize(), objectSummary.getLastModified()));
+            }
+            objects = conn.listNextBatchOfObjects(objects);
+        } while (objects.isTruncated());
 
         return objectList;
     }
 
     public Bucket createBucket(Key key, String bucketName) {
-        String accessKey = key.getAccessKey();
-        String secretKey = key.getSecretKey();
-
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        clientConfig.setProtocol(Protocol.HTTP);
-
-        AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
-
-        conn.setEndpoint(RGW_ENDPOINT);
+        AmazonS3 conn = getClient(key);
 
         return conn.createBucket(bucketName);
     }
 
     public void deleteBucket(Key key, String bucketName) {
-        String accessKey = key.getAccessKey();
-        String secretKey = key.getSecretKey();
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        clientConfig.setProtocol(Protocol.HTTP);
-
-        AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
-        conn.setEndpoint(RGW_ENDPOINT);
-
-        System.out.println(bucketName);
+        AmazonS3 conn = getClient(key);
 
         conn.deleteBucket(bucketName);
+    }
+
+    private synchronized static AmazonS3 getClient(Key key){
+        if(amazonS3 != null){
+            return amazonS3;
+        }
+
+        String accessKey = key.getAccessKey();
+        String secretKey = key.getSecretKey();
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        return amazonS3 = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://object-storage.rook.221.154.134.31.traefik.me:10017/", Regions.DEFAULT_REGION.getName()))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
 }
