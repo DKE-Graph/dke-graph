a701790b1ad331ecb625e2b9364c3a2d2dc1d08e
dlwjdgns3415 <dlwjdgns3415@naver.com>
2023-08-01 15:47:42 +0900

    08 01

--- src/test/java/com/etri/sodasapi/SodasApiApplicationTests.java
+++ src/test/java/com/etri/sodasapi/SodasApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.etri.sodasapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SodasApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.etri.sodasapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SodasApiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
