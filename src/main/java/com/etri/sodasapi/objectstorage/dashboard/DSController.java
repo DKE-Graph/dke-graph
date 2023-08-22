270b2dde2594d5afea8e2cce0b295950e7561a57
HanYoonSoo <hdbstn3055@gmail.com>
2023-08-22 11:42:18 +0900

    Temp

--- src/main/java/com/etri/sodasapi/objectstorage/dashboard/DSController.java
+++ src/main/java/com/etri/sodasapi/objectstorage/dashboard/DSController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/dashboard")
+@RequestMapping("/object-storage")
 public class DSController {
     private final DSService dsService;
 
