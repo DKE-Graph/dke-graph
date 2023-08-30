105fbd2668584da1133bc10fa26a3dc4ebca9c4d
YisakH <drm05003@naver.com>
2023-08-30 14:46:16 +0900

    swagger token 기반 인증 기능 추가

--- src/main/java/com/etri/sodasapi/config/SodasApiConfig.java
+++ src/main/java/com/etri/sodasapi/config/SodasApiConfig.java
@@ -3,6 +3,8 @@ package com.etri.sodasapi.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +17,18 @@ public class SodasApiConfig {
                 .version("0.0.1")
                 .description("Spring Boot를 이용한 Sodas API입니다.");
 
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(info);
     }
 }
