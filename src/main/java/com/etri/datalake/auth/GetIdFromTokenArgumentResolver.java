5d8eec44ff7c41fe04f4a9c928983961d56a7a50
dlwjdgns3415 <dlwjdgns3415@naver.com>
2023-09-13 15:29:47 +0900

    Swagger UI 설명 수정

---
+++ src/main/java/com/etri/datalake/auth/GetIdFromTokenArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.etri.datalake.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+
+public class GetIdFromTokenArgumentResolver implements HandlerMethodArgumentResolver {
+    private final KeycloakAdapter keycloakAdapter;
+
+    public GetIdFromTokenArgumentResolver(KeycloakAdapter tokenProvider){
+        this.keycloakAdapter = tokenProvider;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(GetIdFromToken.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if(request == null){
+            throw new IllegalStateException("No HttpServletRequest found");
+        }
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null){
+//            throw new BaseException(BaseExceptionCode.AUTHORIZATION_HEADER_NULL);
+        }
+
+        return keycloakAdapter.getUserPk(authorizationHeader.substring(7));
+    }
+}
