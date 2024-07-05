baf8732e0126f2756e7651d153c7d8ab5ced7e54
laewonjeong <laewonjeong@kangwon.ac.kr>
2024-07-05 17:27:10 +0900

    test

--- test.cpp
+++ test.cpp
@@ -239,10 +239,11 @@ int main(int argc, char** argv){
             int idx;
             
             std::fill(send[0].begin(), send[0].end(), 0.0);
-            for(size_t i = 0; i < end - start; ++i) {
-                for (size_t neighbor : sliced_graph[i]) {
-                    send_buf_ptr[i] += recv_buffer_ptr[neighbor];
-                }
+            const size_t graph_size = sliced_graph[i].size();
+            const size_t* graph_ptr = sliced_graph[i].data();
+            for(size_t j=0; j<graph_size; j++){
+                const size_t from_page = graph_ptr[j];
+                send_buf_ptr[i] += recv_buffer_ptr[from_page];
             }
             
             normalize(send[0], recv1[0]);
