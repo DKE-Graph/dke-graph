1388798b53cdd590385d1465196f7aa94b1877ed
laewonjeong <laewonjeong@kangwon.ac.kr>
2024-07-05 15:56:09 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -242,8 +242,8 @@ int main(int argc, char** argv){
             std::fill(send[0].begin(), send[0].end(), 0.0);
             for(size_t i = 0; i < end - start; ++i) {
                 for (size_t neighbor : sliced_graph[i]) {
-                    cout << send_buf_ptr[neighbor] << ", " << recv_buffer_ptr[i] << endl;
-                    send_buf_ptr[neighbor] += recv_buffer_ptr[i];
+                    cout << send_buf_ptr[i] << ", " << recv_buffer_ptr[neighbor] << endl;
+                    send_buf_ptr[i] += recv_buffer_ptr[neighbor];
                 }
             }
             normalize(send[0]);
