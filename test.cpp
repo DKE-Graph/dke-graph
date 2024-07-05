201552a1bd7d28b6dc2c34275fc8dd37a68083cb
laewonjeong <laewonjeong@kangwon.ac.kr>
2024-07-05 15:55:10 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -238,8 +238,8 @@ int main(int argc, char** argv){
                 cout << "[INFO]COMPUTE EIGENVECTOR ";
             clock_gettime(CLOCK_MONOTONIC, &begin1);
             int idx;
-            if(step!=0)
-                std::fill(send[0].begin(), send[0].end(), 0.0);
+            
+            std::fill(send[0].begin(), send[0].end(), 0.0);
             for(size_t i = 0; i < end - start; ++i) {
                 for (size_t neighbor : sliced_graph[i]) {
                     cout << send_buf_ptr[neighbor] << ", " << recv_buffer_ptr[i] << endl;
