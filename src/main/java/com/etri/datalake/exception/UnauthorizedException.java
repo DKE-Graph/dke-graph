39ee9e9ac31b4a05468f7dd368c530c83aacbfbd
laewonJeong <laewonjeong@kangwon.ac.kr>
2024-11-19 14:39:07 +0900

    remove files

--- src/main/java/com/etri/datalake/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.etri.datalake.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
-
